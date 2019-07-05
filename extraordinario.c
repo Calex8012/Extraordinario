a8c78b5e5e81a7516ff65b8c403aa6f191aa970e
Calex8012 <42974499+Calex8012@users.noreply.github.com>
2019-07-05 12:56:27 -0500

    agregando interrupcion serial

--- extraordinario.c
+++ extraordinario.c
@@ -1,11 +1,28 @@
-#include <extraordinario.h>
+#include <18F4620.h>
+#fuses HS, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT
+#fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
+#use delay(clock=2000000)
 
-void main()
-{
+int flagSerial=0, flagEcho=0, indice=0;
+char caracter;
 
-	while(TRUE)
-	{
-		//TODO: User Code
-	}
+#define Max_Size_Buffer 9
+char palabra[Max_Size_Buffer];
 
+
+#INT_RDA
+void isr_RDA(void){
+   palabra[indice]=getc();
+   indice++;
+   flagSerial=1;
+   caracter=getc();
+   if(indice>palabra){
+      indice=0;
+   }
+}
+
+void main (void){
+   while(1){
+      
+   }
 }
