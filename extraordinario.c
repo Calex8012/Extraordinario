13524a0e434e271fadb4bdbfe0b82b5538ff1836
Calex8012 <42974499+Calex8012@users.noreply.github.com>
2019-07-05 13:07:08 -0500

    Prueba 1

--- extraordinario.c
+++ extraordinario.c
@@ -3,10 +3,20 @@
 #fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
 #use delay(clock=2000000)
 
+#define __DEBUG_SERIAL__ //Si comentas esta linea se deshabilita el debug por serial y el PIN_C6 puede ser usado en forma digital I/O
+
+#ifdef __DEBUG_SERIAL__
+   #define TX_232        PIN_C6
+   #define RX_232        PIN_C7
+   #use RS232(BAUD=9600, XMIT=TX_232, BITS=8,PARITY=N, STOP=1,RCV=RX_232)
+   #use fast_io(c)
+#endif
+
+
 int flagSerial=0, flagEcho=0, indice=0;
 char caracter;
 
-#define Max_Size_Buffer 9
+#define Max_Size_Buffer 10
 char palabra[Max_Size_Buffer];
 
 
@@ -22,7 +32,17 @@ void isr_RDA(void){
 }
 
 void main (void){
+   set_tris_c(0x80);
+   enable_interrupts(GLOBAL);
+   enable_interrupts(INT_RDA);  
    while(1){
-      
+   
+      if(flagSerial==1){
+         for(int indiceRecorrido=0;indiceRecorrido<indice;indiceRecorrido++){
+            printf("%c",(palabra[indiceRecorrido]));
+            putc(caracter);
+         }  
+         flagSerial=0;
+      }
    }
 }
